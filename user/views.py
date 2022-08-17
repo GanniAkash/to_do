ea62c7b5507539822b2975efcdaac3bc70b05c70
GanniAkash <ganniakash1610@gmail.com>
2022-08-17 18:56:22 +0530

    User app created and implemented

---
+++ user/views.py
@@ -0,0 +1,15 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}')
+            return redirect('todo:todo-home')
+    form = UserRegisterForm()
+    return render(request, 'user/register.html', context={'form': form})
